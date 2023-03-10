fd65a1f1a21be99df6d06e76d369bfd6dadcb509
Centauria <centauriavc@gmail.com>
2023-03-10 23:07:53 +0800

    enable debugging feature

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "src/center.h"
+#include <chrono>
+#include <thread>
 using namespace std;
 
 int main()
 {
+    // Uncomment this to start debugging
+    // std::this_thread::sleep_for(30s);
     Center center;
     center.initialize();
     while (center.refresh())
