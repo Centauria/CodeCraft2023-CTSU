5da6c97ff86ed3ca0ba770f0ebbfc1c21553ed08
Centauria <centauriavc@gmail.com>
2023-03-10 12:16:04 +0800

    use official SDK

---
+++ src/io.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Centauria V. CHEN on 2023/3/10.
+//
+
+#include "io.h"
---
+++ src/io.h
@@ -0,0 +1,8 @@
+//
+// Created by Centauria V. CHEN on 2023/3/10.
+//
+
+#ifndef CODECRAFTSDK_IO_H
+#define CODECRAFTSDK_IO_H
+
+#endif//CODECRAFTSDK_IO_H
