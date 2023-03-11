ca1d6c276bc0df63e7dc095f807b48df21704e75
Hyy <1819588250@qq.com>
2023-03-11 20:09:55 +0800

    fix point to vector2d

--- src/center.h
+++ src/center.h
@@ -11,6 +11,20 @@
 #include "math/point.h"
 #include "robot.h"
 
+class WrokBench
+{
+private:
+    int type;                 // 工作台类型
+    Point coordinate;          //坐标
+    int reaminsProductFrames; // 剩余生产时间
+    int materials;            // 原材料格状态
+    bool product_status;      // 产品格状态
+public:
+    WrokBench(/* args */);
+    ~WrokBench();
+};
+
+
 
 class Center
 {
@@ -23,7 +37,8 @@ public:
 private:
     int currentFrame = 0;
     int deltaFrame = 0;// Difference between this frame and last frame
-    std::vector<Point> workbenches;
+    // std::vector<Point> workbenches;
+    std::vector<Vector2D> workbenches_coordinate;
     std::vector<Robot> robots;
 };
 
