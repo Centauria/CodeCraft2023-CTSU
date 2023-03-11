e22fcfb0caa6aac80e476afe00277c31dd32a2ac
Centauria <centauriavc@gmail.com>
2023-03-11 22:36:28 +0800

    Workbench abstraction

--- src/center.h
+++ src/center.h
@@ -10,20 +10,7 @@
 
 #include "math/point.h"
 #include "robot.h"
-
-class WrokBench
-{
-private:
-    int type;                 // 工作台类型
-    Point coordinate;          //坐标
-    int reaminsProductFrames; // 剩余生产时间
-    int materials;            // 原材料格状态
-    bool product_status;      // 产品格状态
-public:
-    WrokBench(/* args */);
-    ~WrokBench();
-};
-
+#include "workbench.h"
 
 
 class Center
@@ -37,8 +24,7 @@ public:
 private:
     int currentFrame = 0;
     int deltaFrame = 0;// Difference between this frame and last frame
-    // std::vector<Point> workbenches;
-    std::vector<Vector2D> workbenches_coordinate;
+    std::vector<WorkBench> workbenches;
     std::vector<Robot> robots;
 };
 
