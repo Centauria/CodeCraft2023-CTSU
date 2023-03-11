3638303559a19bf6545789c1a17c01e558b16ce1
Hyy <1819588250@qq.com>
2023-03-11 22:49:23 +0800

    add setter and getter function in center

--- src/center.cpp
+++ src/center.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "center.h"
+#include "math/point.h"
 
 
 Center::Center()
@@ -19,10 +20,37 @@ Center::Center()
 void Center::initialize()
 {
     char line[1024];
+    const double row_interval = 0.25;
+    const double col_interval = 0.25;
+    int K = 0;          // 记录工作台数量
+    std::cout<<" start analysis map data "<<std::endl;
+    int i = 0, j = 0;       // 从地图的左上角开始， i 表示行数，j表示列数
     while (fgets(line, sizeof line, stdin))
     {
         if (line[0] == 'O' && line[1] == 'K') break;
         // TODO: Initialize
+        // std::cout<<line<<std::endl;
+        // 判断每一行的信息
+        while(line[j] == '.'){
+            j++;
+        }
+        
+        if(line[j]>='1' && line[j]<='9'){
+            // 读到的字符代表着工作台
+            K++;
+            int type = line[j] - '0';
+            Vector2D coordinate;
+            double tem_x = 0.25 + 0.5 * j;
+            double tem_y = 49.75 - 0.5 * i;
+            coordinate.set_x(tem_x);
+            coordinate.set_y(tem_y);
+            
+        }
+        
+        if(line[j]=='A'){
+            // 读到的字符代表着机器人
+        }
+        
     }
     std::cout << "OK" << std::endl;
     std::flush(std::cout);
@@ -39,6 +67,7 @@ bool Center::refresh()
     {
         if (line[0] == 'O' && line[1] == 'K') break;
         // TODO: Refresh status
+
     }
     return true;
 }
@@ -52,4 +81,42 @@ void Center::step()
     std::cout << "OK" << std::endl;
     std::flush(std::cout);
     currentFrame++;
+}
+
+
+void WrokBench::setType(int type){
+    this->type = type;
+}
+
+int WrokBench::getType(){
+    return type;
+}
+
+
+void WrokBench::setCoordinate(Vector2D coordinate){
+    double x = coordinate.get_x();
+    double y = coordinate.get_y();
+    this->coordinate.set_x(x);
+    this->coordinate.set_y(y); 
+}
+
+
+Vector2D WrokBench::getCoordinate(){
+    return coordinate;
+}
+
+
+void WrokBench::setMaterials(int materials){
+    this->materials = materials;
+}
+
+int WrokBench::getMaterials(){
+    return materials;
+}
+void WrokBench::setProduct_status(bool product_status){
+    this->product_status = product_status;
+}
+
+bool WrokBench::getProduct_status(){
+    return product_status;
 }
--- src/center.h
+++ src/center.h
@@ -15,13 +15,21 @@ class WrokBench
 {
 private:
     int type;                 // 工作台类型
-    Point coordinate;          //坐标
+    Vector2D coordinate;          //坐标
     int reaminsProductFrames; // 剩余生产时间
     int materials;            // 原材料格状态
     bool product_status;      // 产品格状态
 public:
     WrokBench(/* args */);
     ~WrokBench();
+    void setType(int type);
+    int getType();
+    void setCoordinate(Vector2D coordinate);
+    Vector2D getCoordinate();
+    void setMaterials(int materials);
+    int getMaterials();
+    void setProduct_status(bool product_status);
+    bool getProduct_status();
 };
 
 
@@ -38,7 +46,8 @@ private:
     int currentFrame = 0;
     int deltaFrame = 0;// Difference between this frame and last frame
     // std::vector<Point> workbenches;
-    std::vector<Vector2D> workbenches_coordinate;
+    // std::vector<Vector2D> workbenches_coordinate;
+    std::vector<WrokBench> WrokBenchs;
     std::vector<Robot> robots;
 };
 
