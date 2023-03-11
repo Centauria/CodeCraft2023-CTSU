4a7f31914dce532ff324c33b0f17fe3cf838354d
Hyy <1819588250@qq.com>
2023-03-11 17:13:19 +0800

    add some attributes in robot.h

--- src/robot.h
+++ src/robot.h
@@ -22,9 +22,17 @@ public:
     double ETA();
 
 private:
-    uint8_t id;
-    double forward_speed = 0.0;
-    double rotate_speed = 0.0;
+    uint8_t id;         // 所处工作台ID
+    int items_type;               // 携带物品类型
+    double coefficientOfTimeValue; // 时间价值系数
+    double collisionValueFactor;   // 碰撞价值系数
+    double rotate_speed = 0.0;      //  角速度
+    double forward_speed_x = 0.0;   // 线速度 x
+    double forward_speed_y = 0.0;    // 线速度 y
+    double orientation;            // 朝向
+    double coordinate_x;           // 坐标 x
+    double coordinate_y;           // 坐标 y
+    
     // 请根据实际情况添加所需属性和getter、setter
 };
 
