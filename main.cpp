3993b6807dff6193cff7cc26c9f1db828a0dcd20
Centauria <centauriavc@gmail.com>
2023-03-21 16:10:54 +0800

    testing branch

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 bool readUntilOK()
@@ -15,6 +16,36 @@ bool readUntilOK()
     return false;
 }
 
+struct Point {
+    double x, y;
+};
+struct Vector {
+    double x, y;
+};
+
+class Workbench
+{
+public:
+    int type;                   // 工作台类型
+    Point coordinate;           //坐标
+    int product_frames_remained;// 剩余生产时间
+    int material_status;        // 原材料格状态
+    bool product_status;        // 产品格状态
+};
+
+class Robot
+{
+public:
+    int workbench_id;          // 所处工作台id
+    int item_type;             // 携带物品类型
+    double time_val = 0.0;     // 时间价值系数
+    double collision_val = 0.0;// 碰撞价值系数
+    double omega = 0.0;        // 角速度
+    Vector lineSpeed;          // 线速度
+    double orientation;        // 朝向
+    Point coordinate;          // 坐标
+};
+
 int main()
 {
     readUntilOK();
@@ -23,16 +54,32 @@ int main()
     int frameID;
     while (scanf("%d", &frameID) != EOF)
     {
-        readUntilOK();
-        printf("%d\n", frameID);
-        int lineSpeed = 3;
-        double angleSpeed = 1.5;
+        int money, k;
+        cin >> money;
+        cin >> k;
+        Workbench workbenches[k];
+        for (auto &w: workbenches)
+        {
+            cin >> w.type >> w.coordinate.x >> w.coordinate.y >> w.product_frames_remained >> w.material_status >> w.product_status;
+        }
+        Robot robots[4];
+        for (auto &r: robots)
+        {
+            cin >> r.workbench_id >> r.item_type >> r.time_val >> r.collision_val >> r.omega >> r.lineSpeed.x >> r.lineSpeed.y >> r.orientation >> r.coordinate.x >> r.coordinate.y;
+        }
+        //----------------👆inputs--------------
+        string ok;
+        cin >> ok;
+        cout << frameID << endl;
+        int lineSpeed = 5;
+        double angleSpeed = 0;
         for (int robotId = 0; robotId < 4; robotId++)
         {
             printf("forward %d %d\n", robotId, lineSpeed);
             printf("rotate %d %f\n", robotId, angleSpeed);
         }
-        printf("[%d] OK\n", frameID);
+        cerr << robots[0].lineSpeed.x << endl;
+        printf("OK\n");
         fflush(stdout);
     }
     return 0;
