ccb75b745253743d02cf0c4b9e52e2adf2d76a74
Centauria <centauriavc@gmail.com>
2023-03-26 00:44:02 +0800

    arguments!

--- arguments.h
+++ arguments.h
@@ -5,9 +5,28 @@
 #ifndef CODECRAFTSDK_ARGUMENTS_H
 #define CODECRAFTSDK_ARGUMENTS_H
 
-const double IF_ALREADY_PRODUCING = 17.0;   // range(5.0, 25.0, 2)
-const double IF_NOTHING_INSIDE = 11.0;      // range(2.0, 17.0, 2)
-const double AVERAGING_456 = 4.0;           // range(0,5,1)
-const double DONT_FILL_9_WORKBENCH = 6.0;   // range(0,8,1)
-const double AVOIDING_COLLISION = 1.7;      // {0, 1.7, 2.3}
-#endif//CODECRAFTSDK_ARGUMENTS_H
+#include <cmath>
+
+// Robots anti-collision
+const double ANTICOL_NEGLECT_R = 1.0;
+const double ANTICOL_ALERT_R = 8.0;
+const double ANTICOL_WHEEL_K = 1.0;
+const bool ANTICOL_FLEX_WHEEL = true;
+const double ANTICOL_WEIGHT_A = 5.0;
+const double ANTICOL_WEIGHT_B = 1.0;
+
+// Robots driving
+const double DRIVING_P_ERROR_CORRECTION = 0.3;
+const double DRIVING_SPEEDDOWN_A = 4 * M_PI + 1;
+const double DRIVING_SPEEDDOWN_B = -1.0;
+const double DRIVING_TURN_BREAK = 0.5;
+const double DRIVING_WEIGHT_A = 36.1;
+const double DRIVING_WEIGHT_B = 0.1;
+const double DRIVING_THRESHOLD = 1.0;
+
+const double IF_ALREADY_PRODUCING = 17.0;// range(5.0, 25.0, 2)
+const double IF_NOTHING_INSIDE = 11.0;   // range(2.0, 17.0, 2)
+const double AVERAGING_456 = 4.0;        // range(0,5,1)
+const double DONT_FILL_9_WORKBENCH = 6.0;// range(0,8,1)
+const double AVOIDING_COLLISION = 1.7;   // {0, 1.7, 2.3}
+#endif                                   //CODECRAFTSDK_ARGUMENTS_H
