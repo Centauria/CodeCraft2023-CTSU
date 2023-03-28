bd2d71fa6ab19bf57f6e369e41104c176bfada23
Centauria <centauriavc@gmail.com>
2023-03-28 23:04:09 +0800

    magic words

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "center.h"
 #include <chrono>
+#include <cstdio>
 #include <iostream>
 #include <thread>
 using namespace std;
@@ -7,7 +8,10 @@ using namespace std;
 int main()
 {
     // Uncomment this to start debugging
-//       std::this_thread::sleep_for(20s);
+    //       std::this_thread::sleep_for(20s);
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    setbuf(stdout, nullptr);
     Center center;
     center.initialize();
     while (center.refresh())
