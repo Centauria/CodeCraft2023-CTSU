1f050813adc63cb783e5019a4054b2721810f1e3
Centauria <centauriavc@gmail.com>
2023-03-29 23:32:49 +0800

    system

---
+++ system.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Centauria V. CHEN on 2023/3/29.
+//
+
+#include "system.h"
---
+++ system.h
@@ -0,0 +1,12 @@
+//
+// Created by Centauria V. CHEN on 2023/3/29.
+//
+
+#ifndef CODECRAFTSDK_SYSTEM_H
+#define CODECRAFTSDK_SYSTEM_H
+
+class System
+{
+};
+
+#endif//CODECRAFTSDK_SYSTEM_H
