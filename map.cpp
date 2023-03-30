dd6bd0ec86987411cab974f680f6a547ac4e9d09
Centauria <centauriavc@gmail.com>
2023-03-30 22:58:37 +0800

    Map read(102x102)

--- map.cpp
+++ map.cpp
@@ -8,11 +8,19 @@ Map::Map(size_t height, size_t width)
 {
     this->width = width;
     this->height = height;
-    data = std::string(width * height, '.');
+    data = std::string((width + 2) * (height + 2), '#');
+    write_pointer = 1 * (width + 2) + 1;
+    rows_written = 0;
+    distance_from_barriers = std::make_unique<DMatrix>(height, width);
 }
 void Map::append_line(const std::string &line)
 {
-    if (write_pointer >= data.size()) return;
-    line.copy(data.data() + write_pointer, width);
-    write_pointer += width;
+    if (rows_written >= height) return;
+    auto s = std::regex_replace(line, active_obj, ".");
+    s.copy(data.data() + write_pointer, width);
+    write_pointer += (width + 2);
+    rows_written++;
+}
+void Map::refresh_distances()
+{
 }
--- map.h
+++ map.h
@@ -5,9 +5,11 @@
 #ifndef CODECRAFTSDK_MAP_H
 #define CODECRAFTSDK_MAP_H
 
+#include "matrix.h"
 #include <cstdint>
 #include <cstdlib>
 #include <memory>
+#include <regex>
 #include <string>
 
 class Map
@@ -15,12 +17,18 @@ class Map
 public:
     Map(size_t height, size_t width);
     void append_line(const std::string &line);
+    void refresh_distances();
 
 private:
     std::string data;
-    size_t write_pointer = 0;
+    std::unique_ptr<DMatrix> distance_from_barriers;
     size_t width = 0;
     size_t height = 0;
+
+    size_t write_pointer = 0;
+    size_t rows_written = 0;
+
+    const std::regex active_obj{R"([^.#])"};
 };
 
 #endif//CODECRAFTSDK_MAP_H
