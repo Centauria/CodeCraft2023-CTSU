6b3074438c44a1ea974a74a93eca3851c256864e
samuelcai <enguangcai@gmail.com>
2023-04-04 18:51:29 +0800

    寻路基本可以了吧 （胖子不保证能运行）

--- path.cpp
+++ path.cpp
@@ -4,7 +4,9 @@
 
 #include "path.h"
 #include <algorithm>
+#include <cmath>
 #include <cstring>
+#include <iostream>
 #include <math.h>
 #include <queue>
 
@@ -21,6 +23,7 @@ double h(Index start, Index end)
 {
     double dx = start.x - end.x, dy = start.y - end.y;
     return sqrt((dx * dx) + (dy * dy));
+//    return abs(dx)+abs(dy);
 }
 
 std::vector<Index> list_all_neighbors(Index current, int dist)
@@ -40,6 +43,7 @@ std::vector<Index> list_all_neighbors(Index current, int dist)
 
 Path reconstruct_path(Index from[][105], Index start, Index end)
 {
+    std::cerr << "reconstructing" << std::endl;
     Path path;
     Index next = end;
     while (!(from[next.x][next.y] == start))
@@ -54,17 +58,17 @@ Path reconstruct_path(Index from[][105], Index start, Index end)
 
 bool check(Index a, DMatrix &Dmap, int width)
 {
-    if (Dmap(a.y, a.x) == 0) return false;
-    if (Dmap(a.y, a.x) >= 2) return true;
+    if (Dmap(a.y, a.x) == 0) return false;//false 不能过
+    if (Dmap(a.y, a.x) >= 2) return true;//true 能过
     if (width == 2)
     {
-        for (int j = a.y - 1; j <= j + 1; j++)
+        for (int j = -1; j <= 1; j++)
         {
-            for (int i = a.x - 1; i <= i + 1; i++)
+            for (int i = -1; i <= 1; i++)
             {
-                if (j == a.y && i == a.x) continue;
-                if (Dmap(i, a.x - 1) == 0 && Dmap(j, a.x + 1) == 0) return false;
-                if (Dmap(a.y - 1, i) == 0 && Dmap(a.y + 1, j) == 0) return false;
+                if (i == j && i!=0) continue;
+                if (Dmap(a.y + i, a.x - 1) == 0 && Dmap(a.y + j, a.x + 1) == 0) return false;
+                if (Dmap(a.y - 1, a.x + i) == 0 && Dmap(a.y + 1, a.x + j) == 0) return false;
             }
         }
     } else
@@ -104,14 +108,21 @@ bool check(Index a, DMatrix &Dmap, int width)
 Path a_star(GameMap &map, Index start, Index end, int width)
 {
     std::priority_queue<Node> openSet;
+    start.y++;
+    start.x++;
+    end.y++;
+    end.x++;
     openSet.push(Node{0, start});
     Index from[105][105];
     double Gscore[105][105];
-    bool visited[105][105];
+    for(int i = 0; i < 105; i++){
+        for(int j = 0; j < 105; j++){
+            Gscore[i][j] = 99999;
+        }
+    }
+    Gscore[start.x][start.y] = 0;
     DMatrix Dmap = map.get_distances();
-    memset(visited, false, sizeof(visited));
-    visited[start.x][start.y] = true;
-    while (openSet.size())
+    while (!openSet.empty())
     {
         Index current = openSet.top().index;
         openSet.pop();
@@ -119,13 +130,19 @@ Path a_star(GameMap &map, Index start, Index end, int width)
         {
             return reconstruct_path(from, start, end);
         }
-        std::vector<Index> neighbor = list_all_neighbors(current, Dmap(current.y, current.x));
+        std::vector<Index> neighbor = list_all_neighbors(current, std::lround(Dmap(current.y, current.x)));
         for (auto n: neighbor)
         {
-            if (!check(n, Dmap, width) || visited[n.x][n.y]) continue;
-            openSet.push({Gscore[current.x][current.y] + 1 + h(n, end), n});
-            from[n.x][n.y] = current;
-            visited[n.x][n.y] = true;
+            if (!check(n, Dmap, 2)){
+                std::cerr << n.x << ' ' << n.y << std::endl;
+                std::cerr << "这个点过不了" << std::endl;
+                continue;
+            }
+            if(Gscore[n.x][n.y] > Gscore[current.x][current.y] + 1){
+                Gscore[n.x][n.y] = Gscore[current.x][current.y] + 1;
+                openSet.push({Gscore[n.x][n.y] + h(n, end), n});
+                from[n.x][n.y] = current;
+            }
         }
     }
     return {};
