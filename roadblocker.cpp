989239b9f54e67ea805d374665d05eea50073435
Centauria <centauriavc@gmail.com>
2023-04-05 20:22:14 +0800

    block

--- roadblocker.cpp
+++ roadblocker.cpp
@@ -10,19 +10,22 @@ void RoadBlocker::block_wide_road(DMatrix &Dmap)
 }
 void RoadBlocker::block_narrow_road(DMatrix &Dmap)
 {
-    DMatrix temp = convolve(Dmap, Index{3, 3}, [](DView &v) -> double {
-        if (v(1, 1) < 0) return v(1, 1);
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                if (i == j && i != 1) continue;
-                if (v(i, 0) == 0 && v(j, 2) == 0) return -1;
-                if (v(0, i) == 0 && v(2, j) == 0) return -1;
-            }
-        }
-        return v(1, 1);
-    });
-    DView v{Dmap, {1, 1}, {101, 101}};
+    DMatrix temp = convolve(
+            Dmap, Index{3, 3},
+            [](DView &v) -> double {
+                if (v(1, 1) < 0) return v(1, 1);
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (i == j && i != 1) continue;
+                        if (v(i, 0) == 0 && v(j, 2) == 0) return -1;
+                        if (v(0, i) == 0 && v(2, j) == 0) return -1;
+                    }
+                }
+                return v(1, 1);
+            },
+            true);
+    DView v{Dmap, {0, 0}, {100, 100}};
     v = temp;
 }
