687839709a3ddee2d4235a009afd6d31eb7f03ef
Centauria <centauriavc@gmail.com>
2023-04-08 02:04:33 +0800

    delete src

--- src/io.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Centauria V. CHEN on 2023/3/10.
-//
-
-#include "io.h"
--- src/io.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Centauria V. CHEN on 2023/3/10.
-//
-
-#ifndef CODECRAFTSDK_IO_H
-#define CODECRAFTSDK_IO_H
-
-#endif//CODECRAFTSDK_IO_H
